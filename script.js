5bdaae4ab5c30210ea74e46f4355350243591e8c
Maya akter <mayaakter1096@gmail.com>
2025-03-14 12:15:04 +0600

    create div child with js and show in browser

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-
+    let date =  document.getElementById('date');
     const completeButtons = document.querySelectorAll('.complete_btn');
     let taskCount = document.getElementById('task_count');
-   
-    console.log(taskCount.innerText);
+    let number =document.getElementById('number');
+    let parentDiv = document.getElementById('parent_div');
+
+  
+
+    // set date
+    function formatDate(date) {
+        const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+        const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     
+        const dayName = days[date.getDay()];
+        const monthName = months[date.getMonth()];
+        const day = date.getDate();
+        const year = date.getFullYear(); 
+    
+        return `${dayName}, ${monthName} ${day} ${year}`;
+    }
+    const today = new Date();
+    const formattedDate = formatDate(today);
+    date.innerText = formattedDate;
+
+
+
+
+
+
+
+    // 6 btn click
     completeButtons.forEach(button => {
         button.addEventListener('click', function() {
             console.log('click');
@@ -13,7 +38,23 @@
             button.style.backgroundColor = 'gray';
             button.style.cursor = 'not-allowed';
           
-            let count = taskCount.innerText - 1;
-            taskCount.innerText = count;
+            let dicrease = taskCount.innerText - 1;
+            taskCount.innerText = dicrease;
+
+            let currentNumber = parseInt(number.textContent, 10);
+            currentNumber += 1;
+            number.innerText = currentNumber;
+
+            
+
+            let currTime = new Date().toLocaleTimeString();
+            let pera = document.createElement('p');
+            pera.innerText = `You have Complete The Task Add Dark Mode at ${currTime}   `;
+            pera.classList.add('pera','bgc');
+            parentDiv.appendChild(pera);
+           
+
+
+
         });
     });
